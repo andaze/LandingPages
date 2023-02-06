e2439792496838615ea893866aecf350e5071c72
TomoyukiHata <hatayuki-andaze.com>
2023-02-06 15:04:29 +0900

    add second header

--- themes/generic-dx/assets/js/scroll.js
+++ themes/generic-dx/assets/js/scroll.js
@@ -9,3 +9,14 @@ let options = {
 };
 
 let scroll = new smoothScroll('a[href*="#"]', options);
+
+window.addEventListener('scroll', () => {
+    const scroll = window.scrollY;
+    let target = window.pageYOffset + document.getElementById('key-visual').getBoundingClientRect().bottom;
+
+    if (scroll > target) {
+        document.getElementById('fixed-header').style.top = "0";
+    } else {
+        document.getElementById('fixed-header').style.top = "-200px";
+    }
+});
