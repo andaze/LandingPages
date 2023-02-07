2ec24e99fdaf53012b5f260b78c246618312ec6a
TomoyukiHata <hatayuki-andaze.com>
2023-02-07 11:52:48 +0900

    adjust z-index

--- themes/generic-dx/assets/js/scroll.js
+++ themes/generic-dx/assets/js/scroll.js
@@ -10,13 +10,15 @@ let options = {
 
 let scroll = new smoothScroll('a[href*="#"]', options);
 
+const fixed_header = document.getElementById('fixed-header');
+
 window.addEventListener('scroll', () => {
     const scroll = window.scrollY;
     let target = window.pageYOffset + document.getElementById('key-visual').getBoundingClientRect().bottom;
 
     if (scroll > target) {
-        document.getElementById('fixed-header').style.top = "0";
+        fixed_header.style.top = "0";
     } else {
-        document.getElementById('fixed-header').style.top = "-200px";
+        fixed_header.style.top = "-200px";
     }
 });
